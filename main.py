09e34acab6f6e41be3848686972fffdb44413917
duckg <duck@uber-hip.com>
2025-02-16 08:57:29 -0800

    D: added player sprite

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
-    
+
+
     running = True
     while running:
         # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+
         # Fill screen with black color
         screen.fill("black")  # You can also use (0, 0, 0) for black
-        
+
+        player.draw(screen)
+
         # Update the display
         pygame.display.flip()
-    
+
         dt = clock.tick(60) / 1000.0
 
     pygame.quit()
